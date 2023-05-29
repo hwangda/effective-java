ae0ea73cf9b5ba8ff18bed617554768d54a3c091
hdy <hdy0506@gmail.com>
2023-05-29 23:25:02 +0900

    빌더 패턴 - lombok 어노테이션 사용 예제 @AllArgsConstructor(access = AccessLevel.PRIVATE) 추가

--- src/main/java/me/whiteship/chapter01/item02/builder/BuilderTest.java
+++ src/main/java/me/whiteship/chapter01/item02/builder/BuilderTest.java
@@ -6,5 +6,11 @@ public class BuilderTest {
         new NutritionFacts.Builder(10, 10)
                 .calories(10)
                 .build();
+
+//        new NutritionFactsLombok(3, 3, 3, 3, 3, 3);
+        // @AllArgsConstructor(access = AccessLevel.PRIVATE) 일 경우 위 코드 불가능
+        // 아래처럼 Builder를 사용해서만 접근 가능
+        new NutritionFactsLombok.NutritionFactsLombokBuilder()
+                .servingSize(3).build();
     }
 }
--- src/main/java/me/whiteship/chapter01/item02/builder/NutritionFactsLombok.java
+++ src/main/java/me/whiteship/chapter01/item02/builder/NutritionFactsLombok.java
@@ -1,11 +1,14 @@
 package me.whiteship.chapter01.item02.builder;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 // 코드 2-3 빌더 패턴 - 점층적 생성자 패턴과 자바빈즈 패턴의 장점만 취했다. (17~18쪽)
 @Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class NutritionFactsLombok {
-    private final int servingSize;
+    public final int servingSize;
     private final int servings;
     private final int calories;
     private final int fat;
